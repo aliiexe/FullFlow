cee2446fe22e1b438bce67693c03b624cdf8359f
aliiexe <alibusinessbourak@gmail.com>
2025-05-04 22:03:29 +0100

    made some changes in the footer and navbar and changed the line's path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import PricingBuilder from "./sections/PricingBuilder";
 import ProcessSection from "./sections/ProcessSection";
 import AboubakrNavBar from "./ui/AboubakrNav";
 import SlidingFooter from "./ui/FooterWithReveal";
+import FaqSection from "./sections/FaqSection";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-8 relative">
@@ -17,6 +18,7 @@ export default function Home() {
       <ServicesSection />
       <PricingBuilder />
       <ProcessSection />
+      <FaqSection/>
       <SlidingFooter/>
     </main>
   );
